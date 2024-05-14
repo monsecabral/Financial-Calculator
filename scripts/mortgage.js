70156ce2d67b088297fde2a868593878767f4d00
monsecabral <cabral_monse@yahoo.com>
2024-05-14 16:04:41 -0400

    checkpoint

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -2,14 +2,35 @@
 
 window.onload = init;
 
+// text values
+const principalLoanTextBox = document.getElementById("principalLoanTextBox");
+const interestRateTextBox = document.getElementById("interestRateTextBox");
+const yearTextBox = document.getElementById("yearTextBox");
+const outputTextBox = document.getElementById("outputTextBox");
 
+//buttons
+const submitButton = document.getElementById("submitButton");
+const resetButton = document.getElementById("resetButton");
 
-function monthlyPayment(){
+window.onload = init;
 
+function init (){
+    submitButton.onclick = onSubmitButtonClicked;
+    resetButton.onclick = onResetButtonClicked;
 }
 
+function onSubmitButtonClicked(){
+    let userEnterField1 = Number(principalLoanTextBox);
+    let userEnterField2 = Number(interestRateTextBox);
+    let userEnterField3 = Number(yearTextBox);
 
+    let sum = (userEnterField1 * userEnterField2) / userEnterField3;
 
-function interestAmount(){
+    console.log(sum);
 
+    outputTextBox.value = sum;
+}
+
+function onResetButtonClicked(){
+    
 }
